6d8ad684d91e3fa1d4d0043354892cfa565bb268
Raymund Rimando <raymund.rimando@arcticwolf.com>
2013-04-25 19:32:43 -0400

    make the mac address unique by os

--- crust/setup.bash
+++ crust/setup.bash
@@ -3,8 +3,10 @@
 if [[ `uname` == "Darwin" ]];
 then
   export ETHDEV="en0: Wi-Fi (AirPort)"
+  export CRUST_MAC="080027880CA6"
 else
   export ETHDEV="eth0"
+  export CRUST_MAC="080027880CA7"
 fi
 echo "ETHDEV=$ETHDEV"
 alias crust="VAGRANT_CWD=$HOME/private/sandbox/crust vagrant"
