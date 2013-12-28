1724f2d1254a6dc3f5827428649e4318f22a906f
Raymund Rimando <raymund.rimando@gmail.com>
2013-12-27 22:11:37 -0500

    add gflags for remote client

--- python/bin/gcald.py
+++ python/bin/gcald.py
@@ -1,9 +1,11 @@
+import datetime
 import daemon
 import time
 import os
 import logging
 import service
 import sys, time
+import gflags
 
 TASK     = 'GTask'
 CALENDAR = 'GMenu'
@@ -23,9 +25,26 @@ def run():
   with daemon.DaemonContext():
     do_something()
 
-import datetime
+def process_flags(argv):
+  """Uses the command-line flags to set the logging level.
+
+  Args:
+    argv: List of command line arguments passed to the python script.
+  """
+
+  # Let the gflags module process the command-line arguments.
+  try:
+    argv = gflags.FLAGS(argv)
+  except gflags.FlagsError, e:
+    print '%s\nUsage: %s ARGS\n%s' % (e, argv[0], FLAGS)
+    sys.exit(1)
+
+  # Set the logging according to the command-line flag.
+  #logging.getLogger().setLevel(getattr(logging, gflags.FLAGS.logging_level))
+
 
 if __name__ == "__main__":
+  process_flags(sys.argv)
   logging.basicConfig(filename='%s/gcald.log' % os.environ['LOG_DIR'], level=logging.DEBUG)
   SHOPPINGFD = service.ShoppingGenerator(CALENDAR, TASK, server=SERVER, dbname=DBNAME)
   one_week = datetime.timedelta(days=7)
